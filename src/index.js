c34796500440fff3e21c279d1335c13fd0f342a8
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-23 16:20:41 +0100

    Add import store @ index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { store } from './store';
+
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const render = () => {
+	root.render(
+		<React.StrictMode>
+			<App state={store.getState()} dispatch={store.dispatch} />
+		</React.StrictMode>
+	)
+}
+
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
